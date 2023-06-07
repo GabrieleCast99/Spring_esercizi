9c80a729dfeaf3223728b0676943850eb8e92b26
Gabriele <gabriele.castiglione@libero.it>
2023-06-07 17:04:37 +0200

    StateMachine

---
+++ StateMachine/src/test/java/co/develhope/loginDemo/SimpleTest.java
@@ -0,0 +1,20 @@
+package co.develhope.loginDemo;
+
+import co.develhope.loginDemo.order.entities.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SimpleTest {
+
+    @Test
+    public void SimpleTest(){
+        Order order = new Order();
+        //ACCEPT
+        //IN PREPARATION
+        //READY
+        //DELIVERING
+        //COMPLETED
+    }
+
+}
